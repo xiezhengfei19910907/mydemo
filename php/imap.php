20619a32166d1b9e312df1f8b6146e119ab3a584
zfxie <zfxie@i9i8.com>
2017-04-01 18:49:56 +0800

    php imap

--- php/imap.php
+++ php/imap.php
@@ -1,6 +1,37 @@
 <?php
 
-$link = imap_open("{hwimap.exmail.qq.com:993/imap/ssl/novalidate-cert}INBOX", "service@jjshouse.co.uk", "jjsHOUSE!789");
+class InitEmailBox {
+    private static $emailBox = null;
 
-var_dump($link);
-var_dump('test');
+    private function __construct() {
+
+    }
+
+    private function __clone() {
+        // TODO: Implement __clone() method.
+    }
+
+    public static function getEmailBox($config, $email, $password) {
+        if (!is_null(self::$emailBox) && imap_ping(self::$emailBox)) {
+            return self::$emailBox;
+        }
+
+        $mbox = imap_open($config, $email, $password);
+        if (empty($mbox) || !imap_ping($mbox)) {
+            return false;
+        }
+
+        self::$emailBox = $mbox;
+    }
+}
+
+
+$emailBox = InitEmailBox::getEmailBox("{hwimap.exmail.qq.com:993/imap/ssl/novalidate-cert}INBOX", "service@jjshouse.co.uk", "jjsHOUSE!789") or die('连接失败');
+$emailCount = imap_num_msg($emailBox);
+for ($i = $emailCount; $i > 0; $i--) {
+    $emailHeaderInfo = imap_headerinfo($emailBox, $i);
+
+    foreach ($emailHeaderInfo as &$headerInfo) {
+        $headerInfo = imap_mime_header_decode($headerInfo);
+    }
+}
