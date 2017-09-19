20ba61889d36dc7212e5c5cce5ea599fe356f4ec
allen xie <xiezhengfei19910907@gmail.com>
2017-09-19 11:02:54 +0800

    提交

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,1 +1,18 @@
-print("hello world")
+# coding: utf-8
+# print("hello world")
+
+file = open('/Users/allen/file.txt', 'w')
+# file.write('hello, world')
+
+# 注释
+
+# print(type(file))
+
+
+aa = 'sdfdsffdsfsfdsfdsfdsfds  dfds dfdfdfdfdfdfd' \
+     'fdfdfdfdfdfd'
+
+# print(aa)
+
+name = input('name:')
+print('name:{name}'.format(name=name))
