7837bf6a664bd5fb91e71a379522727105eac858
allen <zhengfei.xie@ifchange.com>
2017-11-28 15:17:58 +0800

    更新icdc简历

---
+++ php/updateICDCResumeContent.php
@@ -0,0 +1,278 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+$json = '{
+      "source": {
+        "src": 1,
+        "src_no": " 2N5onCxDnynBZHCwJKHHOw"
+      },
+      "basic": {
+        "updated_at": "2017-11-28 00:00:00",
+        "text": "",
+        "resume_name": "软件测试",
+        "name": "",
+        "gender": "F",
+        "birth": "1984年9月",
+        "work_experience": 10,
+        "marital": "Y",
+        "degree": 1,
+        "address": 321,
+        "account": 321,
+        "political_status": "团员",
+        "self_remark": "本人有 9 年以上测试工作经验，熟悉软件项目管理，精通软件测试流程，对主流测试技术有深刻理解，熟练使用 Jmeter、loadrunner 进行接口、性能测试，熟悉产品自动化测试流程，熟练掌握 Oracle、SQL Server、MySQL等数据库使操作，熟练 Linux 操作系统使用，熟悉 B/S、C/S 架构系统，熟悉 Java、Python 开发语言应用。 \n 本人性格开朗、稳重、有活力，待人热情、真诚；工作认真负责，积极主动，能吃苦耐劳，勇于承受压力，敢于创新；上进心强、勤于学习并能不断提高自身的能力与综合素质。有较强的组织能力和团队协作精神，具有较强的适应能力；纪律性强，工作积极；意志坚强，具有较强的无私奉献精神。 \n在未来的工作中，我将以充沛的精力，刻苦钻研的精神来努力工作，稳定地提高自己的工作能力，与企业同步发展。",
+        "expect_city_names": "陕西",
+        "expect_salary_from": "",
+        "expect_salary_to": "",
+        "current_status": 1,
+        "expect_work_at": "立即上岗",
+        "expect_type": "全职",
+        "expect_position_name": "软件/互联网开发/系统集成",
+        "expect_industry_name": "计算机软件",
+        "avatar_mark": "//mypics.zhaopin.com/pic/2017/09/07/b9ff73d5-3069-4a95-9bf8-66648de80166.jpg",
+        "expect_city_ids": 28,
+        "id": 22947869
+      },
+      "contact": {
+      },
+      "work": [
+        {
+          "start_time": "2017年01月",
+          "end_time": "",
+          "so_far": "Y",
+          "corporation_name": "深圳全丰智控科技有限公司",
+          "position_name": "测试主管",
+          "basic_salary_from": 6,
+          "basic_salary_to": 8,
+          "industry_name": "电子技术/半导体/集成电路",
+          "corporation_type": "民营",
+          "scale": "20人以下",
+          "reporting_to": "",
+          "subordinates_count": "",
+          "annual_salary_from": "",
+          "annual_salary_to": "",
+          "responsibilities": "工作描述：\n完成公司测试流程建立，对产品版本开发各关键过程点进行质量保障；\n负责公司新组建测试团队管理工作，包括但不限于技术能力提升及人员日常事务管理；\n参与完成公司研发产品的具体测试执行工作。"
+        },
+        {
+          "start_time": "2013年05月",
+          "end_time": "2016年12月",
+          "so_far": "N",
+          "corporation_name": "中软国际科技服务有限公司",
+          "position_name": "软件测试",
+          "basic_salary_from": 8,
+          "basic_salary_to": 10,
+          "industry_name": "计算机软件",
+          "corporation_type": "",
+          "scale": "",
+          "reporting_to": "",
+          "subordinates_count": "",
+          "annual_salary_from": "",
+          "annual_salary_to": "",
+          "responsibilities": "工作描述：\n从事网管产品，网优产品的测试设计与执行、维护工作。"
+        },
+        {
+          "start_time": "2009年02月",
+          "end_time": "2013年04月",
+          "so_far": "N",
+          "corporation_name": "文思创新软件技术有限公司",
+          "position_name": "软件测试",
+          "basic_salary_from": 4,
+          "basic_salary_to": 6,
+          "industry_name": "IT服务(系统/数据/维护)",
+          "corporation_type": "上市公司",
+          "scale": "10000人以上",
+          "reporting_to": "",
+          "subordinates_count": "",
+          "annual_salary_from": "",
+          "annual_salary_to": "",
+          "responsibilities": "工作描述：\n文思信息技术有限公司（纽交所上市代码：VIT）成立于1995年，是一家信息技术服务提供商及在中国居领先地位的离岸软件开发公司之一。文思是第一家在纽约股票交易所上市的中国软件服务外包企业。据国际数据公司(IDC)的统计, 以2007年销售额衡量, 文思信息技术在为欧美市场提供离岸软件开发行业中位居中国第一。\n在该公司任职期间，主要从事华为业务部门的工作。先后在华为核心网、软件公司、中研多条产品线参与项目的软件测试工作。从事版本软件测试的整个流程，包括但不限于分析需求、制定测试方案、编写测试用例、执行具体测试、质量分析等工作。"
+        },
+        {
+          "start_time": "2007年07月",
+          "end_time": "2008年12月",
+          "so_far": "N",
+          "corporation_name": "西安域飞科技有限公司",
+          "position_name": "软件测试与技术支持",
+          "basic_salary_from": 2,
+          "basic_salary_to": 4,
+          "industry_name": "IT服务(系统/数据/维护)",
+          "corporation_type": "民营",
+          "scale": "20人以下",
+          "reporting_to": "",
+          "subordinates_count": "",
+          "annual_salary_from": "",
+          "annual_salary_to": "",
+          "responsibilities": "工作描述：\n在公司从事软件测试工作"
+        }
+      ],
+      "project": [
+        {
+          "start_time": "2015年07月",
+          "end_time": "2016年12月",
+          "so_far": "N",
+          "name": "SmartCare SQI项目",
+          "soft_env": "LINUX、Oracle、hadoop数据集群、Windows浏览器等",
+          "develop_tool": "",
+          "describe": "功能描述：SQI解决方案，基于端到端的业务质量监测平台和方法对数据源进行统一监测分析，发现业务质量问题，及时进行优化提升业务质量，增加业务流量。 \n系统描述：SQI基于B/S架构开发，底层通过Hdoop数据集群对海量信令面和用户面进行分析计算，同时将计算结果存储至上层Oracle数据库，然后系统使用者可通过主流浏览器进行业务查询操作等。",
+          "responsibilities": "前端JavaScript技术实现，在前端功能测试中，运用接口测试的方法验证业务参数功能正确性。\n基于业务流程和数据流，通过分层测试、端到端全流程流程的手段，完成产品整体功能测试。\n分层测试中提炼自动化需求，将测试数据基线化。通过编写脚本工具实现分层自动化。"
+        },
+        {
+          "start_time": "2013年11月",
+          "end_time": "2015年06月",
+          "so_far": "N",
+          "name": "Nastar Discovery项目",
+          "soft_env": "LINUX、Oracle、Windows、浏览器等搭建测试环境",
+          "develop_tool": "",
+          "describe": "功能描述：Nastar系统是无线网络性能分析的整体解决方案，可集成其他系统，对现网数据进行收集、分析和入库，从而提供无线性能分析的整体解决方案。 \n系统描述：Nastar采取C/S架构（Discovery采取B/S架构，主流业务同Nastar区别较小，在软件功能实现上两者差异大），服务器侧由多个功能模块组成，这些功能模块与客户端相应模块通信同时各模块通过数据库接口访问Nastar服务器上的数据库，进行数据库操作。",
+          "responsibilities": "负责GSM/UMTS特性业务功能新开发需求测试、低版本老旧功能保障测试、补丁版本上线测试等工作。\n完成维护版本测试数据基线化，手工测试用例修改满足自动化开发需求。完成业务自动化场景提炼等工作。"
+        },
+        {
+          "start_time": "2013年05月",
+          "end_time": "2013年10月",
+          "so_far": "N",
+          "name": "M2000 CME系统",
+          "soft_env": "Linux、Windows、Sybase数据库等。",
+          "develop_tool": "",
+          "describe": "功能描述：CME基站数据配置工具，通过图形化、模板化、向导化等不同方法，提供配置规划、镜像配置、配置策略检查、容错策略等，高效支撑网络调整和网络优化，使无线网络的配置、调整、改造更加安全可靠。针对复杂的基站搬迁场景，CME还特别提供了强大专业的基站搬迁功能，搬迁时邻区自动调整，大大提高了搬迁的速度和准确性。 系统描述：CME有单机版和C/S架构两种版本， 后者可独立运行也可集成与M2000作为其组件运行。",
+          "responsibilities": "负责连线工具及GSM业务配置功能测试及现网补丁版本测试。"
+        },
+        {
+          "start_time": "2010年09月",
+          "end_time": "2013年04月",
+          "so_far": "N",
+          "name": "华为IMANAGER N2000、IMANAGER M2000网管系统",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "iManager N2000为固定网解决方案、iManager M2000为移动网网管解决方案。iManager N2000综合网管系统是针对公司的固定网解决方案推出的一体化网管系统。在网络层次上位于业务管理层，对全网的宽窄带设备提供一体化的管理解决方案。 iManager M2000 移动网网元管理系统（简称M2000）是华为公司移动通信领域的网管系统。",
+          "responsibilities": "N2000主要负责维护版本TL1北向、业务管理等模块的测试工作，M2000主要负责信令网管理（STP）、最差端局监控系统的测试，其中最差端局监控系统为单独的B/S架构产品，数据库采用SQL Server。\n工作期间成果：TL1北向自动化脚本维护更新、信令网管理业务离岸落地、最差端局监控系统项目完整结项。"
+        },
+        {
+          "start_time": "2009年10月",
+          "end_time": "2010年08月",
+          "so_far": "N",
+          "name": "G3阅读器",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "即手持电子书阅读器，是由中国移动定制生产、内置移动2G/3G网络模块的一种专业用来看电子书的设备。",
+          "responsibilities": "完成阅读器软件功能性能测试，与硬件厂商合作完成产品整机测试，完成中国移动研究院入库准入测试。"
+        },
+        {
+          "start_time": "2009年07月",
+          "end_time": "2009年10月",
+          "so_far": "N",
+          "name": "音乐图片下载系统",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "项目描述：系统提供图片、音乐的下载管理工作，并提供相应接口内容至计费系统。",
+          "responsibilities": "负责系统小版本的下载模块以及日志模块的整体测试，编写执行测试用例，记录反馈跟踪测试问题等工作。"
+        },
+        {
+          "start_time": "2009年02月",
+          "end_time": "2009年06月",
+          "so_far": "N",
+          "name": "N2510线路保障系统",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "iManager N2510 是华为公司基于多年对接入网运维的深刻理解而推出的专业线路保障系统，提供线路质量的评估、分析、诊断和优化四大功能。主要应用于在用户接入层为运营商提供铜线、光线路测试及保障功能，满足业务发放前的线路质量预评估，及运营期间的线路业务保证与线路故障定位。",
+          "responsibilities": "参与N2510 Frame项目组的测试工作。根据需求编写测试用例并执行相关的功能、性能、安装测试等，同时跟踪记录测试问题，实施缺陷分析以及问题回溯等工作。"
+        },
+        {
+          "start_time": "2008年06月",
+          "end_time": "2008年11月",
+          "so_far": "N",
+          "name": "电信级用户管理&amp;计费系统（NetCT OSS）",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "该系统主要完成电信业务计费、IP网络管理以及网站访问统计业务。电信业务包括IP网用户和非IP网用户（固话、手机）上网时长、专线流量、网页点击次数等。IP网络管理一切IP设备、一切后台应用、以及Unix工作站。能够灵活添加、修改资费定价信息和促销信息；增、删、查、改用户信息、管理员信息；按月生成用户月帐单；按月、年分别生成月帐务信息统计报表和年帐务信息统计报表并允许用户通过Internet自查询自己当前或历史帐单。",
+          "responsibilities": "负责该系统中的数据提取模块、数据整合系统以及管理员管理模块的功能测试工作。数据提取模块是数据采集系统的中央处理模块，主要测试Client端用户登入登出原始数据到Server端数据库记录的转变及传输是否能够准确完成。原始记录为AAA（验证、授权、计费）服务器的wtmpx文件。网络模块在测试时主要考虑到数据的庞大，网络传输中经常遇到中断等因素，测试数据整合系统是否定时完成明细表到日表、日表到月表、月表到年表的数据汇总。管理员管理模块的测试则涉及帐号的开通、暂停（加锁）恢复、删除，管理员资料管理、权限管理是否未出现异常并按照需求实现相应功能的操作。"
+        },
+        {
+          "start_time": "2008年02月",
+          "end_time": "2008年05月",
+          "so_far": "N",
+          "name": "门禁考勤管理系统",
+          "soft_env": "数据库脚本、门禁前置机安装程序、网站安装程序、会议终端安装程序",
+          "develop_tool": "",
+          "describe": "门禁考勤系统采用B/S结构，系统所有的设置、查询操作都可以通过浏览器操作，具有部署方便、易维护的特点，功能分为三个部分：门禁管理、考勤管理、会议管理，是基于一卡通平台身份子系统的应用系统，是一卡通产品的一个子系统。",
+          "responsibilities": "对整个产品安装调试并进行全面测试，并编写相关测试文档。首先根据研发人员提交的开发计划或需求说明书编写测试计划和测试点文档，根据详细设计和概要设计编写测试用例初稿。在具体进行功能测试、性能测试、安全测试以及安装卸载测试等测试类型的过程中不断修改完善测试用例。待研发人员修改完BUG，再完整的执行一次回归测试。最后在测试结束时提交测试报告、使用说明书等文档。"
+        },
+        {
+          "start_time": "2017年02月",
+          "end_time": "2017年10月",
+          "so_far": "N",
+          "name": "综合管理系统（实验室检测管理、在线考试、数据综合分析）",
+          "soft_env": "",
+          "develop_tool": "",
+          "describe": "该产品主要实现对工厂实验室检测过程的管理，以及对实验结果数据的综合分析功能，其中包括通过在线考试系统对实验室相关工作人员的技能考核。\n该系统均为B/S架构，其中第一版基于Windows+MySQL开发，后期版本基于Linux+Oracle开发实现。",
+          "responsibilities": "协助产品经理完成部分页面的原型设计（通过Axure实现）。\n完成公司测试流程制定并应用与该项目的具体开发过程中。\n完成公司测试用例模版、测试报告模版的制定并应用于该项目中。"
+        }
+      ],
+      "education": [
+        {
+          "start_time": "2003年10月",
+          "end_time": "2007年07月",
+          "so_far": "N",
+          "school_name": "西安邮电学院",
+          "discipline_name": "信息与计算科学",
+          "degree": 1
+        }
+      ],
+      "certificate": [],
+      "training": [],
+      "language": [
+        {
+          "name": "英语",
+          "level": "良好"
+        }
+      ],
+      "skill": [],
+      "is_ecv": 0
+    }';
+
+$resumeParseContent = json_decode($json, true);
+
+
+$worker_name = 'icdc_online';
+$host = '192.168.8.70:4730';
+$client_tmp = new GearmanClient();
+$client_tmp->addServers($host);
+
+
+$param = array(
+    'c' => 'resumes/logic_resume',
+    'm' => 'ebao',
+    'p' => $resumeParseContent,
+);
+
+$request = array(
+    'header'  => array(
+        'uid'     => '1',
+        'uname'   => 'zyh',
+        'version' => '1',
+        'signid'  => '2132',
+        'ip'      => '192.168.8.23',
+        'appid'   => '5',
+        'log_id'  => gen_sign_id(),
+    ),
+    'request' => $param,
+);
+$request = json_encode($request);
+$result = $client_tmp->doNormal($worker_name, $request);
+
+if (GEARMAN_SUCCESS == $client_tmp->returnCode()) {
+    $result = json_decode($result);
+    print_r($result);
+}
+
+
+function gen_sign_id() {
+    $arr = gettimeofday();
+
+    return sprintf('%u', ((($arr['sec'] * 100000 + $arr['usec'] / 10) & 0x7FFFFFFF) | 0x80000000));
+}
+
+
+
+
+
+
